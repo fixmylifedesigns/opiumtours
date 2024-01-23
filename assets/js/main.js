6b5b176cbbc9fc7745df86cebbac0da9c5eba1b7
Irving Duran <ijd.irving@gmail.com>
2024-01-23 18:21:09 +0700

    mvop

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,32 +33,32 @@
 
 	// Nav.
 
-		// Toggle.
-			$(
-				'<div id="navToggle">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
+		// // Toggle.
+		// 	$(
+		// 		'<div id="navToggle">' +
+		// 			'<a href="#navPanel" class="toggle"></a>' +
+		// 		'</div>'
+		// 	)
+		// 		.appendTo($body);
 
 		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+			// $(
+			// 	'<div id="navPanel">' +
+			// 		'<nav>' +
+			// 			$('#nav').navList() +
+			// 		'</nav>' +
+			// 	'</div>'
+			// )
+			// 	.appendTo($body)
+			// 	.panel({
+			// 		delay: 500,
+			// 		hideOnClick: true,
+			// 		hideOnSwipe: true,
+			// 		resetScroll: true,
+			// 		resetForms: true,
+			// 		side: 'left',
+			// 		target: $body,
+			// 		visibleClass: 'navPanel-visible'
+			// 	});
 
 })(jQuery);
